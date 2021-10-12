8d28aa98d25d6805ee06213ee042f4893d495fb5
Anatole <me@anatole.dev>
2021-10-12 10:12:07 +0200

    Switch to ECDSA

--- src/auth/rsa.jwt.go
+++ src/auth/rsa.jwt.go
@@ -1,7 +1,7 @@
 package auth
 
 import (
-	"crypto/rsa"
+	"crypto/ecdsa"
 	"encoding/json"
 	"fmt"
 	"os"
@@ -15,48 +15,55 @@ import (
 /*
 	Generate keyfiles:
 
+	-- ECDSA --
+	openssl ecparam -name prime256v1 -genkey -noout -out Credentials/privkey.pem && \
+	openssl ec -in Credentials/privkey.pem -pubout -out Credentials/pubkey.pem && \
+	openssl req -new -x509 -key Credentials/privkey.pem -out Credentials/ca.pem -days 30
+
+	-- RSA --
 	openssl genrsa -out Credentials/app.rsa 512 && \
 	openssl rsa -in Credentials/app.rsa -pubout > Credentials/app.rsa.pub
+
 */
 
 var (
-	signingKey   *rsa.PrivateKey
-	verifyingKey *rsa.PublicKey
+	signingKey   *ecdsa.PrivateKey
+	verifyingKey *ecdsa.PublicKey
 )
 
 func init() {
 	// Read the signing key
-	signingBytes, err := os.ReadFile("Credentials/app.rsa")
+	signingBytes, err := os.ReadFile("Credentials/privkey.pem")
 	if err != nil {
 		log.WithError(err).Fatal("auth, rsa: cannot read private key")
 	}
 
-	signingKey, err = jwt.ParseRSAPrivateKeyFromPEM(signingBytes)
+	signingKey, err = jwt.ParseECPrivateKeyFromPEM(signingBytes)
 	if err != nil {
 		log.WithError(err).Fatal("auth, rsa: cannot parse private key")
 	}
 
-	verifyingBytes, err := os.ReadFile("Credentials/app.rsa.pub")
+	verifyingBytes, err := os.ReadFile("Credentials/pubkey.pem")
 	if err != nil {
 		log.WithError(err).Fatal("auth, rsa: cannot read public key")
 	}
 
-	verifyingKey, err = jwt.ParseRSAPublicKeyFromPEM(verifyingBytes)
+	verifyingKey, err = jwt.ParseECPublicKeyFromPEM(verifyingBytes)
 	if err != nil {
 		log.WithError(err).Fatal("auth, rsa: cannot parse public key")
 	}
 }
 
-var RSA = rsaClient{}
+var ECDSA = ecdsaClient{}
 
-type rsaClient struct{}
+type ecdsaClient struct{}
 
 type RSAClaim struct {
 	*json.RawMessage
 	jwt.RegisteredClaims
 }
 
-func (rsaClient) Sign(data json.RawMessage) (string, error) {
+func (ecdsaClient) Sign(data json.RawMessage) (string, error) {
 	claims := RSAClaim{
 		&data,
 		jwt.RegisteredClaims{
@@ -69,7 +76,7 @@ func (rsaClient) Sign(data json.RawMessage) (string, error) {
 		},
 	}
 
-	token, err := jwt.NewWithClaims(jwt.GetSigningMethod("RS256"), claims).SignedString(signingKey)
+	token, err := jwt.NewWithClaims(jwt.GetSigningMethod("ES256"), claims).SignedString(signingKey)
 	if err != nil {
 		return "", err
 	}
@@ -77,7 +84,7 @@ func (rsaClient) Sign(data json.RawMessage) (string, error) {
 	return token, nil
 }
 
-func (rsaClient) Verify(t string) (*jwt.Token, error) {
+func (ecdsaClient) Verify(t string) (*jwt.Token, error) {
 	token, err := jwt.Parse(t, func(t *jwt.Token) (interface{}, error) {
 		return verifyingKey, nil
 	})
